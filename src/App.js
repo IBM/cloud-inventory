bcceecb1b7bf518d0929921fbae0909ebd595a77
VSAlmeida <vsalmeida@DESKTOP-PL28679.localdomain>
2021-01-11 11:56:49 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import { Button } from 'carbon-components-react';
+
+function App() {
+  return (
+    <div>
+      <h1>Roi</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
+
+export default App;

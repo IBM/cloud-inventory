063a7e76d8c37489c1bcf73b44e1ad5bef02476d
Luis Falcao <luis.falcao@ibm.com>
2021-01-17 16:38:02 -0300

    Update SideBar.js

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -8,6 +8,12 @@ import {
   SideNavLink,
 } from "carbon-components-react/lib/components/UIShell";
 import {
+  Bee32,
+  Carbon32,
+  Enterprise16,
+  Enterprise32, 
+  DataBase32,
+  InventoryManagement32,
   Fade16,
   VirtualMachine32,
   Vpn32,
@@ -19,15 +25,11 @@ const SideBar = ({ isSideNavExpanded }) => {
   return (
     <SideNav aria-label="Side navigation" isRail expanded={isSideNavExpanded}>
       <SideNavItems>
-        <SideNavMenu renderIcon={Fade16} title="Classic Infrastructure">
+        <SideNavMenu renderIcon={DataBase32} title="Classic Infrastructure">
          <Link to="/vsi/classic">
            <SideNavLink renderIcon={VirtualMachine32}>Virtual Server for Classic</SideNavLink>
          </Link>
         </SideNavMenu>
-        {/*
-        <SideNavLink renderIcon={Archive32}>Bare Metal Server</SideNavLink>
-        <SideNavLink renderIcon={Vpn32}>Virtual Private Network</SideNavLink>
-        <SideNavLink renderIcon={ObjectStorage32}>Object Storage</SideNavLink> */}
       </SideNavItems>
     </SideNav>
   );

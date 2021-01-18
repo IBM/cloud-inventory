de17bbb200d451fec59bb5bc732f354867f8740c
Luis Falcao <luis.falcao@ibm.com>
2021-01-18 18:05:28 -0300

    SideBar updated

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,42 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {
-  SideNav,
-  SideNavMenu,
-  SideNavItems,
-  SideNavLink,
+	SideNav,
+	SideNavMenu,
+	SideNavItems,
+	SideNavLink,
 } from "carbon-components-react/lib/components/UIShell";
-import { DataBase32, VirtualMachine32 } from "@carbon/icons-react";
+import {
+	DataBase32,
+	VirtualMachine32,
+	Table32,
+	VirtualPrivateCloudAlt32,
+	VirtualPrivateCloud32
+} from "@carbon/icons-react";
 
 const SideBar = () => {
-  return (
-    <SideNav aria-label="Side navigation" isRail>
-      <SideNavItems>
-        <SideNavMenu renderIcon={DataBase32} title="Classic Infrastructure">
-          <Link to="/classic/vsi">
-            <SideNavLink renderIcon={VirtualMachine32}>
-              Virtual Server for Classic
-            </SideNavLink>
-          </Link>
-          <Link to="/classic/bm">
-            <SideNavLink renderIcon={VirtualMachine32}>Bare Metal</SideNavLink>
-          </Link>
-        </SideNavMenu>
-        <SideNavMenu renderIcon={DataBase32} title="Virtual Private Cloud">
-          <Link to="/vpc/overview">
-            <SideNavLink renderIcon={VirtualMachine32}>
-              VPC Overview
-            </SideNavLink>
-          </Link>
-          <Link to="/vpc/vsi">
-            <SideNavLink renderIcon={VirtualMachine32}>
-              Virtual Server for VPC
-            </SideNavLink>
-          </Link>
-        </SideNavMenu>
-      </SideNavItems>
-    </SideNav>
-  );
+	return (
+		<SideNav aria-label="Side navigation" isRail>
+			<SideNavItems>
+				<SideNavMenu renderIcon={DataBase32} title="Classic Infrastructure">
+					<Link to="/classic/vsi">
+						<SideNavLink renderIcon={VirtualMachine32}>
+							Virtual Server
+						</SideNavLink>
+					</Link>
+					<Link to="/classic/bm">
+						<SideNavLink renderIcon={Table32}>
+							Bare Metal
+						</SideNavLink>
+					</Link>
+				</SideNavMenu>
+				<SideNavMenu renderIcon={VirtualPrivateCloudAlt32} title="Virtual Private Cloud">
+					<Link to="/vpc/overview">
+						<SideNavLink renderIcon={VirtualPrivateCloud32}>
+							VPCs
+						</SideNavLink>
+					</Link>
+					<Link to="/vpc/vsi">
+						<SideNavLink renderIcon={VirtualMachine32}>
+							Virtual server instances
+						</SideNavLink>
+					</Link>
+				</SideNavMenu>
+			</SideNavItems>
+		</SideNav>
+	);
 };
 
 export default SideBar;

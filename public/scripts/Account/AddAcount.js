d4dcab4a2ee834781d54530d7fae5620a265f271
Luis Falcao <luis.falcao@ibm.com>
2021-01-21 19:21:06 -0300

    Accout updated
    Criacao de diretorio ao criar a conta.

--- public/scripts/Account/AddAcount.js
+++ public/scripts/Account/AddAcount.js
@@ -1,16 +1,19 @@
 const { ipcMain } = require("electron");
 const { encrypt } = require("../Helpers/Crypto");
+const os = require('os');  // Allocating os module
 const fs = require("fs");
 
 ipcMain.on("account:add", async (event, arg) => {
   const newAccount = encrypt(JSON.stringify(arg));
-  const path = `${__dirname}/accounts.json`;
+  const path = `${os.homedir()}/.cloud-inventory/`;
+  const file = `${path}/accounts.json`;
   console.log("Creating a new account");
   try {
-    let accounts = JSON.parse(fs.readFileSync(path, "utf8"));
+    let accounts = JSON.parse(fs.readFileSync(file, "utf8"));
     accounts.push(newAccount);
-    fs.writeFileSync(path, JSON.stringify(accounts), "utf-8");
+    fs.writeFileSync(file, JSON.stringify(accounts), "utf-8");
   } catch {
-    fs.writeFileSync(path, JSON.stringify([newAccount]), "utf-8");
+    fs.mkdirSync(path);
+    fs.writeFileSync(file, JSON.stringify([newAccount]), "utf-8");
   }
 });
--- public/scripts/Account/GetAccounts.js
+++ public/scripts/Account/GetAccounts.js
@@ -1,12 +1,13 @@
 const { ipcMain } = require("electron");
 const { decrypt } = require("../Helpers/Crypto");
+const os = require('os');  // Allocating os module
 const fs = require("fs");
 
 ipcMain.handle("account:get", async (event, arg) => {
   console.log(arg.log);
-  const path = `${__dirname}/accounts.json`;
+  const file = `${os.homedir()}/.cloud-inventory/accounts.json`;
   try {
-    let accounts = JSON.parse(fs.readFileSync(path, "utf8"));
+    let accounts = JSON.parse(fs.readFileSync(file, "utf8"));
 
     accounts.forEach((account, index) => {
       accounts[index] = JSON.parse(decrypt(account));

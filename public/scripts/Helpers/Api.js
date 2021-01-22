2d9dce65561421f152e149a2ce35c6500832e310
VSAlmeida <vsalmeida@DESKTOP-PL28679.localdomain>
2021-01-22 15:23:02 -0300

    Fixed iam api endpoint

--- public/scripts/Helpers/Api.js
+++ public/scripts/Helpers/Api.js
@@ -1,13 +1,10 @@
 const axios = require("axios");
 
 const iamApi = axios.default.create({
-  baseURL: "https://iam.cloud.ibm.com/identity/token",
+  baseURL: "https://iam.cloud.ibm.com/identity",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded",
     Accept: "application/json",
-  },
-  params: {
-    grant_type: "urn:ibm:params:oauth:grant-type:apikey",
   },
 });
 

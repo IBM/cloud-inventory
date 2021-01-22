568de8f4694ae43669b91508d76adbd5aa8b6954
VSAlmeida <vsalmeida@DESKTOP-PL28679.localdomain>
2021-01-22 10:37:16 -0300

    Fixed capitalization

---
+++ src/pages/Classic/VirtualServerClassic/tableHeader.js
@@ -0,0 +1,52 @@
+const headers = [
+  {
+    key: "name",
+    header: "Device Name",
+  },
+  {
+    key: "location",
+    header: "Location",
+  },
+  {
+    key: "deviceType",
+    header: "Type",
+  },
+  {
+    key: "os",
+    header: "Operating System",
+  },
+  {
+    key: "vcpu",
+    header: "vCPU",
+  },
+  {
+    key: "ram",
+    header: "RAM",
+  },
+  {
+    key: "publicIp",
+    header: "Public IP",
+  },
+  {
+    key: "privateIp",
+    header: "Private IP",
+  },
+  {
+    key: "vlans",
+    header: "VLANS",
+  },
+  {
+    key: "created",
+    header: "Created",
+  },
+  {
+    key: "billing",
+    header: "Billing",
+  },
+  {
+    key: "status",
+    header: "Status",
+  },
+];
+
+module.exports = headers;

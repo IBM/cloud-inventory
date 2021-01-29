8d975eeeb51f81bdf1dd79b7a1dcbe926388c7de
VSAlmeida <vinicius.sebadelhe@ibm.com>
2021-01-29 11:40:18 -0300

    Added home page layout

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,27 @@
 import React from "react";
 
+import Imagem from "../../images/image.jpg";
+
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <img
+        className="bx--home__img"
+        src={Imagem}
+        alt="home page illustration"
+      />
+      <div className="bx--home__container--text">
+        <div className="bx--home__text bx--home__title">
+          What is <br />
+          Cloud Inventory?
+        </div>
+        <div className="bx--home__text bx--home__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing it, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+          veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+          ea commodo consequat.
+        </div>
+      </div>
     </div>
   );
 };

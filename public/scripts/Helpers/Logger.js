ef7f340e8e4ecea0e791d2bc074149a88855f544
VSAlmeida <vinicius.sebadelhe@ibm.com>
2021-02-02 16:44:34 -0300

    Added log handling

---
+++ public/scripts/Helpers/Logger.js
@@ -0,0 +1,10 @@
+const logger = require("electron-log");
+const { logsPath } = require("./Archive");
+
+// Sobrescreve o path padrao onde os logs sao
+// armazenados do modulo electron-log
+logger.transports.file.resolvePath = (variables) => {
+  return logsPath + variables.fileName;
+};
+
+module.exports = { logger };

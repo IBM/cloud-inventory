83daa6d1b166408cc52e71065111a1afcb44538e
Luis Falcao <luis.falcao@ibm.com>
2021-02-09 11:23:09 -0300

    inclusao de comentarios

--- public/electron.js
+++ public/electron.js
@@ -6,6 +6,8 @@ require("./scripts/index");
 let mainWindow;
 
 function createWindow() {
+  
+  // Cria a janela do navegador 
   mainWindow = new BrowserWindow({
     center: true,
     minWidth: 1024,
@@ -22,30 +24,41 @@ function createWindow() {
   // Funcao para iniciar maximizado
   mainWindow.maximize();
 
+  // e carrega o index.html do aplicativo 
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.resolve(__dirname, "..", "build", "index.html")}`
   );
 
+  // Abre a ferramenta de desenvolvimento (DevTools)
   if (isDev) {
     mainWindow.webContents.openDevTools();
   }
 
+  // Emitido quando a janela é fechada
   mainWindow.on("closed", () => {
     mainWindow = null;
   });
 }
 
+// Este método será chamado quando o Electron terminar a
+// inicialização e estiver pronto para criar janelas do navegador.
+// Algumas APIs só podem ser usadas após a ocorrência desse evento
 app.on("ready", createWindow);
 
+// Sai quando todas as janelas estão fechadas
 app.on("window-all-closed", () => {
+  // No OS X, é comum que aplicativos e sua barra de menu
+  // permanecam ativos até que o usuário saia explicitamente usando Cmd + Q
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
 app.on("activate", () => {
+  // No OS X, é comum recriar uma janela do aplicativo quando o
+  // ícone do dock é clicado e não há janelas abertas
   if (mainWindow === null) {
     createWindow();
   }

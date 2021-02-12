fa323d40e2bd139aa683251e5a0da7f1b349f857
isabel-bernardo <cristiina.bell@gmail.com>
2021-02-12 11:27:44 -0300

    update-Export

--- public/scripts/Export/PDF.js
+++ public/scripts/Export/PDF.js
@@ -1,7 +1,47 @@
 const { ipcMain } = require("electron");
+const fs = require("fs");
+const PDFDocument = require("./classe");
+const handleCreatePDF = async (arg) => {
+  return new Promise((res, rej) => {
+    const doc = new PDFDocument({
+      layout: "landscape",
+    });
+    //TABELA
+    doc.pipe(fs.createWriteStream(`${arg.exportInfo.path}`));
+    const table = {
+      headers: [],
+      rows: [],
+    };
 
+    arg.headers.forEach((header) => {
+      table.headers.push([header.header]);
+    });
+    arg.rows.forEach((row, index) => {
+      table.rows.push([]);
+      arg.headers.forEach((header) => {
+        table.rows[index].push(row[header.key]);
+      });
+    });
+
+    doc.table(table, 10, 15, {
+      prepareHeader: () => doc.font("Helvetica-Bold").fontSize(9),
+      prepareRow: (row, i) => doc.font("Helvetica").fontSize(7),
+      width: 770,
+    });
+
+    //doc.end();
+    res({ status: "finished" });
+    rej({ status: "error" });
+  });
+};
 ipcMain.handle("exporting:PDF", async (event, arg) => {
-  console.log(arg);
-
-  return "finished";
+  handleCreatePDF(arg)
+    .then((res) => {
+      console.log(res.status);
+      // return res.status;
+    })
+    .catch((err) => {
+      console.log(err.status);
+      //return err.status;
+    });
 });
